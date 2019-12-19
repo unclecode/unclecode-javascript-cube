bb0f8e7c6a3e5ebce810e0a27976d9c259b9c529
kportal-hub <59002469+kportal-hub@users.noreply.github.com>
2019-12-19 12:00:40 +0800

    Update onTestSuccess.js

--- .github/workflows/onTestSuccess.js
+++ .github/workflows/onTestSuccess.js
@@ -125,9 +125,7 @@ async function sendPullToChub(cHub, repo, gitToken, branch) {
 
 const onTestSuccess = async (repo, gitToken, branch) => {
     const cHub = "kportal-hub";
-    console.log(repo)
-    return true
-//    return await sendPullToChub(cHub, repo, gitToken, branch)
+    return await sendPullToChub(cHub, repo, gitToken, branch)
 }
 
 onTestSuccess(process.argv[2], process.argv[3], process.argv[4]).then((res) => {
