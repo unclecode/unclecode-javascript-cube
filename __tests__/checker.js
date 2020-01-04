029205aa0ddaeeb4f13b9db76636b7f8679a04ed
ntohidi <nasrin@kidocode.com>
2020-01-04 10:51:35 +0800

    update

--- __tests__/checker.js
+++ __tests__/checker.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const fs = require('fs')
-const chalk = require('chalk');
 
 class MyReporter {
 	constructor(globalConfig, options) {
@@ -40,15 +39,7 @@ class MyReporter {
 		}
 
 		fs.writeFileSync(process.cwd() + "/__tests__/result.json", JSON.stringify(result, null, 4))
-
-		result.suites.forEach(s => {
-			let testLog = s.test.map(t => chalk.bold(`${t.status ? chalk.green('Passed') : chalk.red('Failed')}: ${chalk.white(t.fullName)}`)).join("\n\r");
-			let txt = chalk.bold(`\n${chalk.bold.cyan(s.describe)}\nTotal: ${s.total} ${chalk.green('Passed:')} ${chalk.green(s.passed)} ${chalk.red('Failed:')} ${chalk.red(s.failed)}\n${testLog}`);
-			console.log(txt)
-		});
-
 		// console.dir(result, {depth: null, colors: true})
-
 	}
 }
 
